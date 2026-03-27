f72d13d7ba29ad00df5f180645e7f3ef4b78408d
Jibril Olajuwon <jibrilolajuwon2006@gmail.com>
2026-03-27 14:31:55 +0100

    blank ix

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/trax-demo/', // Fix for GitHub Pages blank screen
   server: {
     port: 5173,
     host: 'localhost', // Explicitly use IPv4 loopback
@@ -13,4 +14,15 @@ export default defineConfig({
     tailwindcss(),
     react(),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // code-splitting for large dependencies
+          }
+        }
+      }
+    }
+  }
 })
